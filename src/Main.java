ea0d9b436bf0bce7b65698dc3219efb9bdb6117f
Margarita Popova <popova.m.a514@gmail.com>
2023-04-06 16:31:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 7_300;
+        int quantity = 20;
+
+        int miles = cost / quantity;
+
+        System.out.println("Бонусные мили: " + miles);
+    }
+}
